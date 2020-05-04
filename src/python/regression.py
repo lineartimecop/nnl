c0441547e14ac482f8d3f982ddfbeef617eb3473
codesmeller <ecucester@gmail.com>
2020-05-04 10:20:51 +0200

    Regression with Tensorflow updated

--- src/python/regression.py
+++ src/python/regression.py
@@ -9,19 +9,19 @@ X, Y = nnl.very_wiggly_data()
 X_norm = nnl.normalize(X)
 Y_norm = nnl.normalize(Y)
 
-# Linear function
+# Linear function (ca. 1s to train)
 #nn = nnl.NN(depth = 1, width = 2)
 
 #training_mse = nn.train(X_norm, Y_norm, learning_rate = 0.12, epochs = 100)
 #training_mse = nn.train(X_norm, Y_norm, learning_rate = 0.12, epochs = 100, batch_size = 2)
 
-# Non-linear function
+# Non-linear function (ca. 4s to train)
 #nn = nnl.NN(depth = 3, width = 5)
 
 #training_mse = nn.train(X_norm, Y_norm, learning_rate = 0.025, epochs = 3000)
 #training_mse = nn.train(X_norm, Y_norm, learning_rate = 0.1, epochs = 3000, batch_size = 16)
 
-# Very wiggly function
+# Very wiggly function (ca. 16 mins to train)
 nn = nnl.NN(depth = 5, width = 10)
 
 training_mse = nn.train(X_norm, Y_norm, learning_rate = 0.01, epochs = 30000, printing = True)
